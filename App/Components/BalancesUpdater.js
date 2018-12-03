9af115e28a5c9e8c145a32ccfbf2f7d3a9d45374
Jaron Rosenau <jaron.rosenau@gmail.com>
2018-12-02 17:23:09 -0800

    Update BalancesUpdater.js

--- App/Components/BalancesUpdater.js
+++ App/Components/BalancesUpdater.js
@@ -105,7 +105,7 @@ class BalancesUpdater {
     }
 
     setCurrentBlock(next) {
-        return this.rpcRepository.getnetworkinfo((err, block) => {
+        return this.rpcRepository.getblockchaininfo((err, block) => {
 
             if (err) {
                 return next(err);
